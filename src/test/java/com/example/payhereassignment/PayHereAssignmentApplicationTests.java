0ac9cb157be9c9b92fdf1194394af164e2de00ec
jin <jchj108@naver.com>
2022-08-16 22:29:12 +0900

    Initial commit

---
+++ src/test/java/com/example/payhereassignment/PayHereAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.payhereassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayHereAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
